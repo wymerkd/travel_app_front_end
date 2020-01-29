d9b98526f8bb0835884bfe4438bb9d1c17357995
Student <example@example.com>
2020-01-29 09:27:55 -0800

    create initial files, add starter gems, initiate active resource for reviews.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,40 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+    json_response(@reviews)
+  end
+
+  def show
+    @review = Review.find(params[:id])
+    json_response(@review)
+  end
+
+  def create
+    @review = Review.create!(review_params)
+    json_response(@review, :created)
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update!(review_params)
+      render status: 200, json: {
+        message: "This review has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def review_params
+    params.permit(:author, :content, :city_name, :country_name, :rating)
+  end
+end
---
+++ app/models/review.rb
@@ -0,0 +1,3 @@
+class Review < ActiveResource::Base
+  self.site = "http://localhost:3000/reviews"
+end
