beed7b9fe34fc43f1aa5f566cbaa26e94347313e
Student <example@example.com>
2020-01-29 11:13:13 -0800

    add request.params for show method in reviews.controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,17 +3,21 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
     # binding.pry
-    json_response(@reviews)
+    # json_response(@reviews)
   end
 
   def show
-    @review = Review.find(params[:id])
-    json_response(@review)
+    id_number = request.params["id"].to_i
+    @review = Review.find(id_number)
+
+
+    # @review = Review.find(:all, :params => :id )
+    # json_response(@review)
   end
 
   def create
     @review = Review.create!(review_params)
-    json_response(@review, :created)
+    # json_response(@review, :created)
   end
 
   def update
@@ -36,6 +40,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.permit(:author, :content, :city_name, :country_name, :rating)
+    params.permit(:author, :content, :city_name, :country_name, :rating, :id)
   end
 end
