b5c93587684e2df18442a7c8d48c33d22b864d89
Student <example@example.com>
2020-01-30 11:36:08 -0800

    add jwt token to review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
+# ActionController::HttpAuthentication::Token
+
 class Review < ActiveResource::Base
   self.site = "http://localhost:3000/"
+  self.headers['Authorization'] = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1ODA0OTQ3Nzl9.24_twHyU0NBkT6kfpGhp6pGxTcF3k03Yiff0q_HQoi8"
 end
